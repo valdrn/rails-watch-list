9ad63f8663d3c22002be0cf65081f84902acc894
Valentin Derrien <derrien.v@gmail.com>
2022-02-17 16:10:43 +0100

    No front

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     @list.save
-    redirect_to list_path(@list)
+    redirect_to lists_path
   end
 
   def list_params
