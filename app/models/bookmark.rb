054a9a305cdfca4e0e32ba5326937e03fd5aa2e1
Valentin Derrien <derrien.v@gmail.com>
2022-02-18 12:22:16 +0100

    app done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_one_attached :photo
 
   validates :comment, length: { minimum: 6 }
   validates_uniqueness_of :movie_id, :scope => [:list_id]
